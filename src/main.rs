d7fa6a74012b53a5fa915898068a6e683c0e5605
Dung Ngo <dung.ngo@eastagile.com>
2022-12-13 11:37:24 +0700

    crawl events up until the latest saved event

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use serde_json::{json, Value};
 use sqlx::postgres::PgPoolOptions;
 
 const SUI_DEVNET_FULLNODE: &str = "https://fullnode.devnet.sui.io:443";
-const ENTRY_PER_PAGE: usize = 3;
-const MAX_PAGE: usize = 10;
+const ENTRIES_PER_PAGE: usize = 1000;
+const SORT_BY_LATEST: bool = true;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -18,16 +18,16 @@ async fn main() -> Result<(), anyhow::Error> {
         .connect(&database_url)
         .await?;
 
-    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
-    let _row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
+    let last_timestamp = sqlx::query!("SELECT max(timestamp) FROM modules;")
         .fetch_one(&pool)
         .await?;
+    let last_timestamp = last_timestamp.max;
+    let mut crawling = true;
 
     let client = reqwest::Client::new().post(SUI_DEVNET_FULLNODE);
     let mut next_page = Value::Null;
-    
-    for _ in 0..MAX_PAGE {
+
+    while crawling {
         let response = client
             .try_clone()
             .unwrap()
@@ -36,12 +36,10 @@ async fn main() -> Result<(), anyhow::Error> {
                 "id": 1,
                 "method": "sui_getEvents",
                 "params": [
-                    {
-                        "EventType": "Publish"
-                    },
+                    { "EventType": "Publish" },
                     next_page,
-                    ENTRY_PER_PAGE,
-                    true
+                    ENTRIES_PER_PAGE,
+                    SORT_BY_LATEST
                 ]
             }))
             .send()
@@ -60,10 +58,31 @@ async fn main() -> Result<(), anyhow::Error> {
             let package_id = object["event"]["publish"]["packageId"]
                 .as_str()
                 .ok_or_else(|| anyhow::anyhow!("Response does not contain `packageId`."))?;
+            let tx_digest = object["txDigest"]
+                .as_str()
+                .ok_or_else(|| anyhow::anyhow!("Response does not contain `txDigest`."))?;
             let timestamp = object["timestamp"]
                 .as_u64()
                 .ok_or_else(|| anyhow::anyhow!("Response does not contain `timestamp`."))?;
             println!("{sender} published {package_id} at {timestamp}");
+
+            if let Some(last_ts) = last_timestamp {
+                if timestamp <= last_ts as u64 {
+                    crawling = false;
+                    break;
+                }
+            }
+
+            // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+            let _row = sqlx::query!(
+                "INSERT INTO modules(package_id, sender, tx_digest, timestamp) VALUES ($1, $2, $3, $4)",
+                package_id,
+                sender,
+                tx_digest,
+                timestamp as i64
+            )
+            .execute(&pool)
+            .await?;
         }
         println!("{next}");
 
